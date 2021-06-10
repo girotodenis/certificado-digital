99cb74b8ebbeac5d160224eaaf290b0f802c3334
Denis Giroto <girotodenis@gmail.com>
2021-06-10 16:34:06 -0300

    thema

--- ui-a3-seletor/src/main/java/br/com/dsg/uia3/LeguiCertificate.java
+++ ui-a3-seletor/src/main/java/br/com/dsg/uia3/LeguiCertificate.java
@@ -1,5 +1,7 @@
 package br.com.dsg.uia3;
 
+import org.liquidengine.legui.theme.Themes;
+
 import br.com.dsg.legui.controller.StartLeGui;
 import br.com.dsg.legui.controller.eventos.EventAdicionarItemMenu;
 import br.com.dsg.uia3.certificado.ControllerCertificado;
@@ -9,6 +11,8 @@ public class LeguiCertificate {
 	public static void main(String[] args) {
 
 		StartLeGui.get(600, 300, "Certificado A3")
+		.setThemeMenu(Themes.FLAT_PETERRIVER_DARK)
+		.setThemeConteudo(Themes.FLAT_DARK)
 		.abrirFecharMenuPadrao()
 		.addItemMenu(
 				new EventAdicionarItemMenu(
